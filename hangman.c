0ab431f45365879825f7a9461d7524918e91e52f
MaheenShakeel <156514967+MaheenShakeel@users.noreply.github.com>
2024-01-16 23:59:34 +0500

    Update hangman.c

--- hangman.c
+++ hangman.c
@@ -16,13 +16,13 @@ void displayword(struct players *player) {
     int i,j;
     printf("Lives:");
     for(i=0; i<player->lives; i++) {
-        printf("❤️ ");
+        printf("â¤ï¸ ");
     }
     printf("\n");
     printf("Word type: %s\n",player->noun);
     for(i=0; i<strlen(player->word); i++) {
         if(player->lives>0) {
-            if(player->word==' '){
+            if(player->word[i]==' '){
                 printf(" %c",player->word[i]);
             }
             else{
@@ -59,7 +59,7 @@ void updateword(struct players *player,char guess[]) {
     clearScreen();
     printf("Lives:");
     for(i=0; i<player->lives; i++) {
-        printf("❤️ ");
+        printf("â¤ï¸ ");
     }
     printf("\n");
     printf("Word Type: %s\n",player->noun);
@@ -75,7 +75,7 @@ void updateword(struct players *player,char guess[]) {
     if(incomplete==0) {
         player->lives=0;
         player->wins=player->wins+1;
-        printf("Congratulations 🎉🎉 %s You guessed the word \"%s\" correct\n", player->name, player->word);
+        printf("Congratulations ðŸŽ‰ðŸŽ‰ %s You guessed the word \"%s\" correct\n", player->name, player->word);
         sleep(2);
         }
     else if(player->lives==0){
@@ -84,16 +84,16 @@ void updateword(struct players *player,char guess[]) {
     }
 }
 void guess(struct players *player) {
-    char wordguess[30],correctguess[15]= {0};
+    char wordguess[30],correctguess[15]= {0},wrongguess[15]={0};
     int i;
-    int j=0;
+    int j=0,k=0;
     while(player->lives>0) {
         printf("Guess Character/Word:");
         gets(wordguess);
         convertToUpperCase(&wordguess);
         if (strlen(wordguess) > 1) {
             if (strcmp(wordguess, player->word) == 0) {
-                printf("Congratulations 🎉🎉 %s You guessed the word \"%s\" correct\n", player->name, player->word);
+                printf("Congratulations ðŸŽ‰ðŸŽ‰ %s You guessed the word \"%s\" correct\n", player->name, player->word);
                 sleep(2);
                 player->wins = player->wins + 1;
                 player->lives = 0;
@@ -106,14 +106,19 @@ void guess(struct players *player) {
             }
         }
         else {
-            if(strchar(correctguess,wordguess[0])>=0) {
+            if(strchar(correctguess,wordguess[0])>=0 || strchar(wrongguess,wordguess[0])>=0) {
                 printf("You already guessed this character \n");
+                sleep(2);
+                clearScreen();
+                updateword(player,correctguess);
             }
             else if (strchar(player->word, wordguess[0]) >= 0) {
                 correctguess[j]=wordguess[0];
                 j++;
                 updateword(player, correctguess);
             } else {
+                wrongguess[k]=wordguess[0];
+                k++;
                 player->lives = player->lives - 1;
                 updateword(player, correctguess);
             }
@@ -127,7 +132,7 @@ void initialiseGame() {
     gets(player[0].name);
     printf("Enter Name Player2: ");
     gets(player[1].name);
-    sleep(2);
+    sleep(1);
     player[0].wins=0;
     player[1].wins=0;
     while(player[1].wins<3 && player[0].wins<3) {
